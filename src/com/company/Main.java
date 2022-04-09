ededecceb16b0b3fce6dc5e04bdec82bbc176ca8
bradpappan <brad.pappan2@gmail.com>
2022-04-09 12:01:29 -0500

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+import helper.JDBC;
+
+public class Main {
+
+    public static void main(String[] args) {
+        JDBC.openConnection();
+        JDBC.closeConnection();
+    }
+}
