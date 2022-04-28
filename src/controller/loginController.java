bccb297a20898a9ce4a799ccd934fc92164c99d8
bradpappan <brad.pappan2@gmail.com>
2022-04-28 11:29:34 -0500

    added alert for appointments within 15 minutes of login

--- src/controller/loginController.java
+++ src/controller/loginController.java
@@ -1,18 +1,25 @@
 package controller;
 
 import helper.loginPageQuery;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
+import model.appointmentModel;
 
+import javax.swing.*;
 import java.net.URL;
 import java.sql.SQLException;
 import java.util.ResourceBundle;
 import java.time.ZoneId;
 
 public class loginController implements Initializable {
+
+    private ObservableList<appointmentModel> appointments = FXCollections.observableArrayList();
 
     @FXML private TextField usernameTf;
     @FXML private TextField passwordTf;
@@ -22,11 +29,33 @@ public class loginController implements Initializable {
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
+        try {
+            appointments = loginPageQuery.appointmentWarning();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+
         initializeZoneId();
         // Changed to lambda expression
         loginBtn.setOnAction(event -> {
             try {
                 loginPageQuery.selectUser(event, usernameTf.getText(), passwordTf.getText());
+
+                if (appointments.isEmpty()) {
+                    Alert alertError = new Alert(Alert.AlertType.ERROR);
+                    alertError.setTitle("Attention");
+                    alertError.setHeaderText("No appointments within 15 minutes");
+                    alertError.setContentText("There are currently no appointments within 15 minutes of current login.");
+                    alertError.showAndWait();
+                } else {
+                    for (appointmentModel appt : appointments) {
+                        Alert alertError = new Alert(Alert.AlertType.ERROR);
+                        alertError.setTitle("Attention");
+                        alertError.setHeaderText("List of appointments");
+                        alertError.setContentText("Appointments within 15 minutes are: Appointment ID = " + appt.getAppointmentId() + ", Starts at " + appt.getAppointmentStart().toString());
+                        alertError.showAndWait();
+                    }
+                }
             } catch (SQLException e) {
                 e.printStackTrace();
             }
--- src/helper/loginPageQuery.java
+++ src/helper/loginPageQuery.java
@@ -1,5 +1,7 @@
 package helper;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -8,17 +10,24 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.stage.Stage;
+import model.appointmentModel;
 
 import java.io.IOException;
 import java.net.URL;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Objects;
 import java.util.ResourceBundle;
 
 
-public class loginPageQuery implements Initializable {
+public class loginPageQuery {
 
     public static void selectUser(ActionEvent event, String username, String password) throws SQLException {
         PreparedStatement preparedStatement = null;
@@ -41,9 +50,9 @@ public class loginPageQuery implements Initializable {
                     }
                 }
             }
-        } catch(Exception e){
-                e.printStackTrace();
-            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     public static void loginSuccessful(ActionEvent event) throws Exception {
@@ -68,9 +77,33 @@ public class loginPageQuery implements Initializable {
         alertError.showAndWait();
     }
 
-    @Override
-    public void initialize(URL url, ResourceBundle resourceBundle) {
+    public static ObservableList<appointmentModel> appointmentWarning() throws SQLException {
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
 
+        ObservableList<appointmentModel> appointmentWarningOb = FXCollections.observableArrayList();
+        String sql = "SELECT * FROM appointments WHERE Start BETWEEN now() AND date_add(now(), INTERVAL 15 MINUTE);";
+        preparedStatement = JDBC.connection.prepareStatement(sql);
+        //preparedStatement.setTimestamp(1, startTime);
+        //preparedStatement.setTimestamp(2, endTime);
+        resultSet = preparedStatement.executeQuery();
+        while (resultSet.next()) {
+            String appointmentId = resultSet.getString("Appointment_ID");
+            String appointmentTitle = resultSet.getString("Title");
+            String appointmentDescription = resultSet.getString("Description");
+            String appointmentLocation = resultSet.getString("Location");
+            String appointmentType = resultSet.getString("Type");
+            Timestamp appointmentStart = resultSet.getTimestamp("Start");
+            Timestamp appointmentEnd = resultSet.getTimestamp("End");
+            int appointmentCustomerId = resultSet.getInt("Customer_ID");
+            int appointmentUserId = resultSet.getInt("User_ID");
+            String appointmentContactId = resultSet.getString("Contact_ID");
+
+            appointmentModel appointmentResult = new appointmentModel(appointmentId, appointmentTitle, appointmentDescription, appointmentLocation, appointmentType, appointmentStart,
+                    appointmentEnd, appointmentCustomerId, appointmentUserId, appointmentContactId);
+            appointmentWarningOb.add(appointmentResult);
+        }
+        return appointmentWarningOb;
     }
 }
 
